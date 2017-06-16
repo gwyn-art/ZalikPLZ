0363d1e8f2252ea3a31f8c32717189f28af7e161
Elena Vasylenko <elena.vasylenko@gmail.com>
2017-06-16 13:36:04 +0300

    main menu done

---
+++ ZalikPLZ/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class ExitButton : MonoBehaviour {
+	
+	public UnityEvent signalOnClick = new UnityEvent();
+
+	public void _onClick() {
+		this.signalOnClick.Invoke ();
+	}
+
+	void Start(){
+		signalOnClick.AddListener(this.onPlay);
+	}
+
+	void onPlay() { 
+		Debug.Log ("Quit the game");
+		Application.Quit();
+	}
+}
---
+++ ZalikPLZ/Assets/Scripts/StartButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+	public UnityEvent signalOnClick = new UnityEvent();
+
+	public void _onClick() {
+		this.signalOnClick.Invoke ();
+	}
+
+	void Start(){
+	  signalOnClick.AddListener(this.onPlay);
+	}
+
+	void onPlay() { 
+	SceneManager.LoadScene("Classroom");
+	}
+
+}
