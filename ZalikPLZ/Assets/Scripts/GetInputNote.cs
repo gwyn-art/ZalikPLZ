b1ab19aa1c4f01fd8b01371b3a4be07f41d9ece7
Elena Vasylenko <elena.vasylenko@gmail.com>
2017-06-24 17:15:44 +0300

    input field done

---
+++ ZalikPLZ/Assets/Scripts/GetInputNote.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class GetInputNote : MonoBehaviour {
+
+	void Start (){
+		var input = gameObject.GetComponent<InputField>();
+		var se= new InputField.SubmitEvent();
+		se.AddListener(SubmitName);
+		input.onEndEdit = se;
+	}
+
+	private void SubmitName(string arg0){
+		Debug.Log(arg0);
+	}
+}
