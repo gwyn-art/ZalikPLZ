54b792d42b4036ad6a085ccd09e6cdff0df1e6d7
Elena Vasylenko <elena.vasylenko@gmail.com>
2017-06-24 22:50:45 +0300

    rating added

---
+++ ZalikPLZ/Assets/Scripts/ChangeRating.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeRating : MonoBehaviour {
+	
+	public Text rtext;
+
+	void Start () {
+		rtext = GetComponent<Text>();
+		changeRating (100);
+	}
+
+	void changeRating(int newRating){
+		rtext.text = "Your rating: " + newRating;
+	
+	}
+}
