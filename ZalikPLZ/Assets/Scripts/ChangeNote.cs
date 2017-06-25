9bc2dbee4c6a9fb4e8a38e73a9c31e7584b6266f
Elena Vasylenko <elena.vasylenko@gmail.com>
2017-06-25 17:38:19 +0300

    note and date fields added

---
+++ ZalikPLZ/Assets/Scripts/ChangeNote.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeNote : MonoBehaviour {
+
+	public TextMesh note;
+
+	//there will be 2 methods of changing note: load from json(into distedu) and load from input field(into credit book)
+		void change() {
+			note.text = "100";
+		}
+		
+	// Use this for initialization
+	void Start () {
+		change ();
+	}
+}
