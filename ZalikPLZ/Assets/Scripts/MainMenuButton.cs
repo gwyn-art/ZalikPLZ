8aacbcbbdcf156f14782569d61637752873ee715
bon-art <bon-art@mail.ua>
2017-06-27 10:14:17 +0300

    почему я пушу то что вроде делала Лена? что вообщепроисходит :с

---
+++ ZalikPLZ/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour {
+	
+		public UnityEvent signalOnClick = new UnityEvent();
+
+		public void _onClick() {
+			this.signalOnClick.Invoke ();
+		}
+
+		void Start(){
+			signalOnClick.AddListener(this.onPlay);
+		}
+
+		void onPlay() { 
+			SceneManager.LoadScene("MainMenu");
+		}
+		
+
+	}
